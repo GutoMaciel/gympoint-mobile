4d59616879e0dc21863827d8b0fb56c7ac8e51e6
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2020-01-08 14:07:10 -0300

    Feature: Sign In Page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import { Image } from 'react-native';
+// import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/8-layers.png';
-
 import Background from '~/components/Background/BackgroundSignIn';
 
-import { Container } from './styles';
+import { Container, Form, FormInput, SubmitButton } from './styles';
 
 export default function SignIn() {
+  // const [id, setId] = useState('');
+
+  // const dispatch = useDispatch();
+
+  // const loading = useSelector(state => state.student.loading);
+
+  // const handleSubmit = () => {
+  //   // dispatch(signInRequest(id));
+  // }
+
+
   return (
     <Background>
       <Container>
         <Image source={logo} />
+        <Form>
+          <FormInput
+            autoCorrect={false}
+            keyboardType="numeric"
+            placeholder="Your student ID"
+            // returnKeyType="send"
+            // onSubmitEditing={handleSubmit}
+            // value={id}
+            // onChangeText={setId}
+            />
+            <SubmitButton onPress={() => {}}>
+              Sign In
+            </SubmitButton>
+        </Form>
       </Container>
     </Background>
   );
