18230b07a395eb91e8df57767d5a607bd9eb99b4
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2020-01-18 11:34:44 -0300

    Chore: Back Icon at stack navigation

--- src/components/Checkin/index.js
+++ src/components/Checkin/index.js
@@ -5,12 +5,6 @@ import { formatRelative, parseISO } from 'date-fns';
 import { Container, CheckinNumber, CheckinDate } from './styles';
 
 export default function Checkin({checkin, index, totalRows}) {
-  // const response = await api.get(`/students/${studentId}/checkins`);
-
-  // const createdAt = response.data.rows.map(check => ({
-  //   created_at: formatRelative(parseISO(check.created_at), new Date())
-  // }))
-
 
   const dateFormatted = useMemo(() => {
     return formatRelative(parseISO(checkin.createdAt), new Date());
